9bea51bfa21ead7e1d28e1b710b6812e62f103d3
TamilarasanG17 <tamilarasang17@gmail.com>
2026-05-17 12:23:53 +0530

    first commit

---
+++ src/main/java/com/example/shipmenttrackingportal/ShipmenttrackingportalApplication.java
@@ -0,0 +1,13 @@
+package com.example.shipmenttrackingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmenttrackingportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmenttrackingportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shipmenttrackingportal/ShipmenttrackingportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shipmenttrackingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmenttrackingportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
