26c0136e8ac1bed81c0aa34196f9e660f1730c30
TamilarasanG17 <tamilarasang17@gmail.com>
2026-05-17 12:44:43 +0530

    feat: Create repository classes for shipment module

---
+++ src/main/java/com/example/shipmenttrackingportal/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.shipmenttrackingportal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.shipmenttrackingportal.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
