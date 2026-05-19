21ac913ae848e43f5ac2232f169af919b0606fdb
TamilarasanG17 <tamilarasang17@gmail.com>
2026-05-19 12:36:54 +0530

    feat : implement DTO for Regidter

---
+++ src/main/java/com/example/shipmenttrackingportal/dto/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.example.shipmenttrackingportal.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import com.example.shipmenttrackingportal.enums.Role;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
+    @Size(min = 2, max = 100, message = "Full name must be between 2 and 100 characters")
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotNull(message = "Role is required. Must be SHIPPER or CARRIER")
+    private Role role;
+}
